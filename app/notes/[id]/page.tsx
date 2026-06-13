2e0866017abf3961e13b87718d59ec54aa8e5990
Katargina <katrin-kate@ukr.net>
2026-06-13 21:17:58 +0200

    update

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 
-import { fetchNoteById } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api/serverApi";
 import NotePreview from "@/app/@modal/(.)notes/[id]/NotePreview.client";
 
 interface NoteDetailsPageProps {
