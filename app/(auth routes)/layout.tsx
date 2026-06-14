6c16a37a1e9d6f2d976f8850fc9c36b863cb3825
Katargina <katrin-kate@ukr.net>
2026-06-14 12:53:54 +0200

    fixed

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,5 +1,27 @@
-import type { ReactNode } from "react";
+// app/(auth routes)/layout.tsx
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+"use client";
+
+import { useEffect, useState, startTransition } from "react";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function PublicLayout({ children }: Props) {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    // 1. Примусово оновлюємо серверні дані
+    router.refresh();
+
+    // 2. Безпечно оновлюємо стан loading
+    startTransition(() => {
+      setLoading(false);
+    });
+  }, [router]);
+
+  return <>{loading ? <div>Loading...</div> : children}</>;
 }
--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import type { User } from "@/types/note";
+import { User } from "../../types/user";
 
 interface AuthStore {
   user: User | null;
